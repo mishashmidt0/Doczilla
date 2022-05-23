197440418a18220dd6ff88ae479eca0f8ef8a7d4
Misha Shmidt <mishashmidt0@gmail.com>
2022-05-23 05:33:20 +0300

    feat: add create dataBase

--- src/server/bdPostgres/dataBase.js
+++ src/server/bdPostgres/dataBase.js
@@ -1,12 +1,34 @@
 const pg = require('pg');
+require('dotenv').config();
 
 const Pool = pg.Pool
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    password: '123',
+    password: process.env.PGPASSWORD,
     port: 5432,
     database: 'students',
+});
+pool.connect();
+
+const query = ` 
+ create TABLE student(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(255),
+   shortDesc VARCHAR(255),
+   description VARCHAR(255),
+   isDone boolean,
+   date DATE
+);
+ `;
+
+pool.query(query, (err, res) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Table is successfully created');
+    pool.end()
 });
 
 module.exports = pool
